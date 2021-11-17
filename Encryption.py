620f2f7a7ed8b747f7ed408cfc31272d408d5edd
Mohamed Ashraf <43587732+MohmedAshrf@users.noreply.github.com>
2021-11-17 23:19:03 +0200

    Update and rename Security codes.py to Encryption.py

--- Encryption.py
+++ Encryption.py
@@ -28,45 +28,6 @@ while(True):
     print("\nthe cipher is :\n",*cipher_text,sep='')
 
 
-# ## Decryption
-
-# In[5]:
-
-
-import string
-import sys
-
-def n_alpha(char):
-    return ord(char)-97 # "a" start from 97
-
-key=3 # in cieser
-alpha = list(string.ascii_lowercase) #[a,b,c,d,....z]
-cipher_text= list(map(n_alpha,input("Please enter the plain text(or c to cancel): \n").lower().replace(" ", ""))) #convert apha into numeric
-plain_text = []
-for i in cipher_text:
-    if (i>=0) and (i<=25-key): #alpha 
-        plain_text.append(alpha[i-key]) #cieser shift (convert numeric into alpha)
-    elif i>25-key:
-        plain_text.append(alpha[(i%26-key)%26])
-    else:
-        print("invalid input please enter a text from a to z")
-print("\nthe cipher is :\n",*plain_text,sep='')
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
 
 
 
